8bd1b13c8400df4208a6b27b0c36e38c4a3a4c01
ycyjason <jasonyeung216@gmail.com>
2025-02-03 22:12:33 +0800

    Reset

--- js/backToTop.js
+++
@@ -1,9 +0,0 @@
-const backToTopButton = document.getElementById("backToTop");
-
-backToTopButton.addEventListener("click", (e) => {
-    e.preventDefault(); // Prevent default link behavior (jump to top)
-    window.scrollTo({
-        top: 0,
-        behavior: "smooth"
-    });
-});
